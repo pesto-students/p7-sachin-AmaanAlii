bf97cf6909b6229e3c14507c28c95d0cceb2354b
AmaanAlii <amaans.officialmail@gmail.com>
2022-12-03 11:57:06 +0530

    Added Assignment - 3.2 - Call, Bind and Apply

---
+++ Week-3/Assignment3.2/main3.2.js
@@ -0,0 +1,44 @@
+//.call Function
+console.log("\n \n\n Example of .call Function\n\n\n")
+function printaddress(){
+    var address = [
+        this.hno,
+        '\nStreet- ',
+        this.street,
+        '\nCity- ',
+        this.city
+    ].join('');
+    console.log(address);
+}
+
+var amaanadd = {
+    hno: '34', street: 'Gautama Budha Road', city: 'Lucknow'
+}
+printaddress.call(amaanadd); //calling printaddress function with a separate object
+
+//
+//
+//
+//
+
+//.bind Function
+console.log("\n \n\n Example of .bind Function\n\n\n")
+
+function product(a,b){
+    return this.a * this.b;
+}
+
+const productB = product.bind({a: 40, b: 30}); // Binding a and b as an object to the 'this' object
+
+console.log(productB());
+
+//
+//
+//
+//
+//
+console.log("\n \n\n Example of .apply Function\n\n\n")
+var arrayofnumbers = [9,8,5,4,2,10,7,6,];
+
+var max = Math.max.apply(null, arrayofnumbers); // Calling Math.max while passing an array using .apply
+console.log('Largest No. - ', max);
